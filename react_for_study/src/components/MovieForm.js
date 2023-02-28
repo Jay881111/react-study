699219d1c909ce34d2638a8addb8e44b937c3812
Jay881111 <kpdrm88@gmail.com>
2023-02-28 20:58:00 +0900

    add delete function

--- react_for_study/src/components/MovieForm.js
+++ react_for_study/src/components/MovieForm.js
@@ -3,12 +3,13 @@ import "./MovieForm.css";
 import ShowMovie from "./ShowMovie";
 
 function MovieForm() {
+  const [newMovieTitle, setNewMovieTitle] = useState("");
+  const [newMovieYear, setNewMovieYear] = useState("");
+
   const [movies, setMovies] = useState([
     { key: 1, title: "Home Alone", year: 1997 },
     { key: 2, title: "Fast & Furious", year: 2015 },
   ]);
-  const [newMovieTitle, setNewMovieTitle] = useState("");
-  const [newMovieYear, setNewMovieYear] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,37 +19,47 @@ function MovieForm() {
     ]);
   };
 
+  const deleteHandler = (key) => {
+    setMovies(
+      movies.filter((movie) => {
+        return movie.key !== key;
+      })
+    );
+  };
+
   const showMovieList = (
     <ul>
-      <ShowMovie movies={movies} />
+      <ShowMovie movies={movies} deleteHandler={deleteHandler} />
     </ul>
   );
 
   return (
     <div className="Movie">
       <h1>What is your favorite movie?</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="movieyear">
-          Movie Title
-          <input
-            value={newMovieTitle}
-            onChange={(e) => setNewMovieTitle(e.target.value)}
-            type="text"
-            placeholder="영화이름을 입력하세요"
-          />
-        </label>
-        <label htmlFor="movieyear">
-          Movie Year
-          <input
-            name="movieyear"
-            value={newMovieYear}
-            onChange={(e) => setNewMovieYear(e.target.value)}
-            type="number"
-            placeholder="개봉연도를 입력하세요"
-          />
-        </label>
+      <div className="MovieForm">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="movieyear">
+            Movie Title
+            <input
+              value={newMovieTitle}
+              onChange={(e) => setNewMovieTitle(e.target.value)}
+              type="text"
+              placeholder="영화이름을 입력하세요"
+            />
+          </label>
+          <label htmlFor="movieyear">
+            Movie Year
+            <input
+              name="movieyear"
+              value={newMovieYear}
+              onChange={(e) => setNewMovieYear(e.target.value)}
+              type="number"
+              placeholder="개봉연도를 입력하세요"
+            />
+          </label>
+        </form>
         <button>add</button>
-      </form>
+      </div>
       {showMovieList}
     </div>
   );
--- react_for_study/src/components/ShowMovie.js
+++ react_for_study/src/components/ShowMovie.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-function ShowMovie({ movies }) {
-  return (
-    <>
-      {movies.map((el) => {
-        return (
-          <li key={el.key}>
-            {el.title}
-            <span>({el.year})</span>
-          </li>
-        );
-      })}
-    </>
-  );
+function ShowMovie({ movies, deleteHandler }) {
+  const showMovie = movies.map((el) => {
+    return (
+      <li key={el.key}>
+        {el.title}
+        <span>({el.year})</span>
+        <button
+          className="show_button"
+          onClick={() => {
+            deleteHandler(el.key);
+          }}
+        >
+          삭제
+        </button>
+      </li>
+    );
+  });
+  return <>{showMovie}</>;
 }
 
 export default ShowMovie;
