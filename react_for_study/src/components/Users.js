9a3cd7fcaefcdd2d26337e508e0928b9ca46a237
Jay881111 <kpdrm88@gmail.com>
2023-03-02 13:20:11 +0900

    position

--- react_for_study/src/components/Users.js
+++ react_for_study/src/components/Users.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import UserList from "./UserList";
-import Spinner from "./Spinner";
-import "./Users.css";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import UserList from './UserList';
+import Spinner from './Spinner';
+import './Users.css';
 
 const Users = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setLoading(true);
-    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
-      setUsers(response.data);
-      setLoading(false);
-    });
-  }, []);
-  console.log("users", users);
+    useEffect(() => {
+        setLoading(true);
+        axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+            setUsers(response.data);
+            setLoading(false);
+        });
+    }, []);
+    console.log('users', users);
 
-  return (
-    <div>
-      <h1>UserLists</h1>
-      <div>{loading ? <Spinner /> : <UserList users={users} />}</div>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-center">UserLists</h1>
+            <div>{loading ? <Spinner /> : <UserList users={users} />}</div>
+        </div>
+    );
 };
 
 export default Users;
