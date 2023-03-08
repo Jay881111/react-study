d2a3eef6884159f8983ac6d62890f8307aab9fe2
Jay881111 <kpdrm88@gmail.com>
2023-03-08 12:22:19 +0900

    padding

--- react_for_study/src/components/Main.js
+++ react_for_study/src/components/Main.js
@@ -1,36 +1,36 @@
-import React from "react";
-import "./Main.css";
+import React from 'react';
+import './Main.css';
 
 function Main() {
-  return (
-    <div>
-      <h1>공부한 내용 정리</h1>
-      <div className="Main">
-        <ul>
-          <p>0. Main</p>
-          <li>Router</li>
-        </ul>
-        <ul>
-          <p>1. Movie</p>
-          <li>usestate</li>
-          <li>props</li>
-        </ul>
-        <ul>
-          <p>2. Contact</p>
-          <li></li>
-          <li></li>
-        </ul>
-        <ul>
-          <p>3. User</p>
-          <li>BootStrap</li>
-          <li>Axios</li>
-          <li>Props</li>
-          <li>Map</li>
-          <li>useParams</li>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-center mt-5">공부한 내용 정리</h1>
+            <div className="Main">
+                <ul>
+                    <p>0. Main</p>
+                    <li>Router</li>
+                </ul>
+                <ul>
+                    <p>1. Movie</p>
+                    <li>useState</li>
+                    <li>props</li>
+                </ul>
+                <ul>
+                    <p>2. Contact</p>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <ul>
+                    <p>3. User</p>
+                    <li>BootStrap</li>
+                    <li>Axios</li>
+                    <li>Props</li>
+                    <li>Map</li>
+                    <li>useParams</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Main;
